c69ddf04c573ad3600ce237eecada12addf5b511
kevinccbsg <kevinccbsg@gmail.com>
2023-11-15 00:14:25 +0100

    feat: add control to script execution

--- index.js
+++ index.js
@@ -9,22 +9,28 @@ const main = async (path) => {
   logger.info(`Importing file ${path}...`);
   const { players, matches, results } = await importDataFromCSV(path);
   logger.info(`File ${path} imported: ${players.length} players; ${matches.length} matches; ${results.length} records...`);
-  for (let index = 0; index < players.length; index += 1) {
-    const player = players[index];
-    await upsertPlayer(player.Players);
+  if (process.env.USER) {
+    for (let index = 0; index < players.length; index += 1) {
+      const player = players[index];
+      await upsertPlayer(player.Players);
+    }
+    logger.info('all players imported/updated...');
   }
-  logger.info('all players imported/updated...');
-  for (let index = 0; index < matches.length; index += 1) {
-    const match = matches[index];
-    await upsertEvent(match.eventName, match.eventYear);
-    await upsertMatch(match.eventName, match.eventYear, match.id, match.winner, match.loser);
+  if (process.env.MATCHES) {
+    for (let index = 0; index < matches.length; index += 1) {
+      const match = matches[index];
+      await upsertEvent(match.eventName, match.eventYear);
+      await upsertMatch(match.eventName, match.eventYear, match.id, match.winner, match.loser);
+    }
+    logger.info('all events/matches imported/updated...');
   }
-  logger.info('all events/matches imported/updated...');
-  for (let index = 0; index < results.length; index += 1) {
-    const result = results[index];
-    await loadResult(result);
+  if (process.env.RESULTS) {
+    for (let index = 0; index < results.length; index += 1) {
+      const result = results[index];
+      await loadResult(result);
+    }
+    logger.info('success importing all results...');
   }
-  logger.info('success importing all results...');
 };
 
 module.exports = main;
